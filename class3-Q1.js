d5c6719070570c0772200b195d3613a45e278dfb
Jie Chen <79719408+jiechen919293@users.noreply.github.com>
2021-04-19 00:17:06 -0500

    frommac the q1 for class3
    practice the inherican

--- class3-Q1.js
+++ class3-Q1.js
@@ -1,3 +1,6 @@
+// Create a new constructor called MusicVideo that inherits from the 
+// Video constructor.This constructor should also take in an artist argument.
+// Your MusicVideo prototype should inherit from the Video prototype.
 // Add a method to the MusicVideo prototype called watch() that
 //  uses console.log to output a string like "You were Rick Rolled, again!".
 // Create an instance of Video and call the play and watch methods
@@ -8,23 +11,16 @@ function Video(params) {
     this.uploader = [];
     this.seconds = [];
 }
-Video.prototype.play = function() {
+Video.prototype.play() = function(params) {
     console.log('You played the video!');
 }
-Video.prototype.watch = function() {
+Video.prototype.watch() = function(params) {
     console.log('You watched every cat video on the internet!');
 }
 
 function MusicVideo() {
     this.artist = '';
-    Video.call(this);
-}
-MusicVideo.prototype.watch = function() {
-    console.log('You were Rick Rolled, again!');
+    Object.Video.call(this)
 }
 const video = new Video();
 const musicVideo = new MusicVideo();
-video.watch();
-video.play();
-musicVideo.watch();
-musicVideo.play();
